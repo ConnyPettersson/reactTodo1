3a71db25561f7c93d0d3676ed4e54c7552c95a68
PedalConny <pedalconny@hotmail.com>
2024-03-21 12:44:44 +0100

    added html and css

--- reacttodo1/src/App.js
+++ reacttodo1/src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
 
+//Vi definerar en funktion (en react-komponent) som 
+//pratar med index.js som renderar App
+//React kör funktionen App som jämför med index.js id'root'
+//som då uppdaterar DOM (index.html)
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <h1 class="app__title">My ToDo</h1>
+    <p class="todoCounter">
+      0 completed
+    </p>
+    <form class="todoForm">
+      <input class="todoForm__input" type="text"/>
+      <button class="todoForm__submitButton" type="submit">OK</button>
+    </form>
+    <ul class="todoList">
+    </ul>
     </div>
   );
 }
